d39312ad3b7a582885cfa0f7c739a0ee3a4da479
root <root@localhost.localdomain>
2019-05-25 17:16:24 +0530

    version 2.4-stable

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # dark_crawler
-#### *version stable-1.2*
+#### *version 2.4-stable*
 
 Dork scanner and exploiting framework
 
@@ -11,11 +11,11 @@ This tool scans different search engines using a dork and return URL's correspon
 
 1. Python 3 or above
 
-### On Linux
+#### Linux
 ```
 apt-get install python3
 ```
-### On Windows
+#### Windows
 * [python-3.7](https://www.python.org/downloads/) - Python Version 3.7
 
 2. Install requirements
@@ -23,7 +23,7 @@ apt-get install python3
 pip install -r requirements.txt
 ```
 
-## How to Execute
+## Usage
 
 #### Linux
 ```
@@ -33,23 +33,27 @@ python3 dark_cralwer.py -h
 ```
 dark_crawler.py -h
 ```
-## Options
+##### Options
 ```
--  -h, --help            show this help message and exit
+  -h, --help            show this help message and exit
   -d DORK, --dork=DORK  String to search [eg:- index.php?id=]
-  --engine=bing         Search engine to use google,bing,all
+  --engine=bing         Search engine to use [google,bing,webcrawler]
   --pages=2             Number of pages to scan
-  --domain=DOMAIN       Specify domain for specific results
+  --domain=DOMAIN       Specify domain for specific results eg IN,example.com
   --output=file.txt     Save ouput to file
   --threads=2           Number of parallel processes
 
-  Misc:
-    --filter            Turn on URL filter(gives specific result)
+  Injection Test:
+    --sqli              Checks gathered urls for classic sql injection
+    --custom-payload=   Custom payload to test against URLs
 
+  Misc:
+    --no-filter         Turn Off URL filter(gives interesting results)
+    -v                  Display error messages on screen
 ```
-## Example
+#### Example
 ```
-python3 dark_crawler.py -d inurl:index.php?id= --pages=5 --threads=7
+python3 dark_crawler.py -d inurl:index.php?id= --pages=5 --threads=7 --sqli
 ```
 
 # Devoloped By
--- dark_crawler.py
+++ dark_crawler.py
@@ -8,11 +8,15 @@ from functools import partial
 from  time import time as timer
 import re
 from urllib.parse import urlparse
+import urllib.request , urllib.error
 from multiprocessing import Pool, freeze_support
+import multiprocessing
 import textwrap
 import random
 from random import randint
 from time import sleep
+from urllib.error import HTTPError, URLError
+import socket
 
 
 
@@ -57,6 +61,157 @@ def filter(list) :
             filter_res.append(x)
     final = dict((urlparse(u).netloc, u) for u in filter_res).values()
     return final
+
+class sqli :
+    def inject(list, custom_payload, verbose) :
+        sleep(1)
+        msg.info('Running basic SQL Injection')
+        sleep(1)
+        msg.info('Displaying Confirmed urls with SQLI ')
+        sleep(1)
+        if custom_payload != None :
+            payload = custom_payload
+        else :
+            payload = "'"
+        vuln_list = []
+        for x in list :
+            try :
+                resp = urllib.request.urlopen(x+payload, timeout=10)
+            except HTTPError as error:
+                if verbose == True :
+                    msg.error(str(error)+' In '+x)
+                    log_error(str(error)+' In '+x)
+                else :
+                    log_error(str(error)+' In '+x)
+            except URLError as error:
+                if isinstance(error.reason, socket.timeout):
+                    if verbose == True :
+                        msg.error('socket timed out - URL '+ x)
+                        log_error('socket timed out - URL '+ x)
+                    else :
+                        log_error('socket timed out - URL '+ x)
+            except socket.error as emsg:
+                if verbose == True :
+                    msg.error(emsg + ' In ' + x)
+                    log_error(emsg + ' In ' + x)
+                else :
+                    log_error(str(emsg) + ' In ' + x)
+            hits = str(resp.read())
+            if str("error in your SQL syntax") in hits:
+                print(x + " is vulnerable --> MySQL Classic")
+                vuln_list.append(x)
+                pass
+            elif str("mysql_fetch") in hits:
+                print(x + " is Vulnerable --> MiscError")
+                vuln_list.append(x)
+                pass
+            elif str("num_rows") in hits:
+                print(x + " is Vulnerable --> MiscError2")
+                vuln_list.append(x)
+                pass
+            elif str("ORA-01756") in hits:
+                print(x + " is Vulnerable --> Oracle")
+                vuln_list.append(x)
+                pass
+            elif str("Error Executing Database Query") in hits:
+                print(x + " is Vulnerable --> JDBC_CFM")
+                vuln_list.append(x)
+                pass
+            elif str("SQLServer JDBC Driver") in hits:
+                print(x + " is Vulnerable --> JDBC_CFM2")
+                vuln_list.append(x)
+                pass
+            elif str("OLE DB Provider for SQL Server") in hits:
+                print(x + " is Vulnerable --> MSSQL_OLEdb")
+                vuln_list.append(x)
+                pass
+            elif str("Unclosed quotation mark") in hits:
+                print(x + " is Vulnerabe --> MSSQL_Uqm")
+                vuln_list.append(x)
+                pass
+            elif str("ODBC Microsoft Access Driver") in hits:
+                print(x + " is Vulnerable --> MS-Access_ODBC")
+                vuln_list.append(x)
+                pass
+            elif str("Microsoft JET Database") in hits:
+                print(x + " is Vulnerable --> MS-Access_JETdb")
+                vuln_list.append(x)
+                pass
+            elif str("Error Occurred While Processing Request") in hits:
+                print(x + " is Vulnerable --> Processing Request")
+                vuln_list.append(x)
+                pass
+            elif str("Microsoft JET Database") in hits:
+                print(x + " is Vulnerable --> MS-Access JetDb")
+                vuln_list.append(x)
+                pass
+            elif str("Error Occurred While Processing Request") in hits:
+                print(x + " is Vulnerable --> Processing Request ")
+                vuln_list.append(x)
+                pass
+            elif str("Server Error") in hits:
+                print(x + " is Vulnerable --> Server Error")
+                vuln_list.append(x)
+                pass
+            elif str("ODBC Drivers error") in hits:
+                print(x + " is Vulnerable --> ODBC Drivers error")
+                vuln_list.append(x)
+                pass
+            elif str("Invalid Querystring") in hits:
+                print(x + " is Vulnerable --> Invalid Querystring")
+                vuln_list.append(x)
+                pass
+            elif str("OLE DB Provider for ODBC") in hits:
+                print(x + " is Vulnerable --> OLE DB Provider for ODBC")
+                vuln_list.append(x)
+                pass
+            elif str("VBScript Runtime") in hits:
+                print(x + " is Vulnerable --> VBScript Runtime")
+                vuln_list.append(x)
+                pass
+            elif str("ADODB.Field") in hits:
+                print(x + " is Vulnerable --> ADODB.Field")
+                vuln_list.append(x)
+                pass
+            elif str("BOF or EOF") in hits:
+                print(x + " is Vulnerable --> BOF or EOF")
+                vuln_list.append(x)
+                pass
+            elif str("ADODB.Command") in hits:
+                print(x + " is Vulnerable --> ADODB.Command")
+                vuln_list.append(x)
+                pass
+            elif str("JET Database") in hits:
+                print(x + " is Vulnerable --> JET Database")
+                vuln_list.append(x)
+                pass
+            elif str("mysql_fetch_array") in hits:
+                print(x + " is Vulnerabe --> mysql_fetch_array")
+                vuln_list.append(x)
+                pass
+            elif str("Syntax error") in hits:
+                print(x + " is Vulnerable --> Syntax error")
+                vuln_list.append(x)
+                pass
+            elif str("mysql_numrows()") in hits:
+                print(x + " is Vulnerable --> mysql_numrows()")
+                vuln_list.append(x)
+                pass
+            elif str("GetArray()") in hits:
+                print(x + " is Vulnerable --> GetArray()")
+                vuln_list.append(x)
+                pass
+            elif str("FetchRow()") in hits:
+                print(x + " is Vulnerable --> FetchRow()")
+                vuln_list.append(x)
+                pass
+            elif str("Input string was not in a correct format") in hits:
+                print(x + " is Vulnerable --> Input String Error")
+                vuln_list.append(x)
+                pass
+            else:
+                pass
+        return vuln_list
 
 
 class google :
@@ -194,13 +349,26 @@ class google_c :
         result = list( set( result ) )
         return result
 
+def log_error(error) :
+    with open('errors', 'a') as file :
+        file.write(error + '\n')
+    file.close()
 
 def save_output(results, filename) :
-    list = results
     with open(filename, 'w') as file:
         for x in results :
             file.write(x + '\n')
     file.close()
+
+def append_sqli(results) :
+    with open('sqli_confirmed', 'a') as file :
+        for x in results :
+            file.write(x+'\n')
+    file.close()
+
+def print_list(list) :
+    for x in list :
+        print(x)
 
 def banner() :
     print(Fore.YELLOW + Style.BRIGHT +"""
@@ -209,7 +377,7 @@ def banner() :
  ═╩╝┴ ┴┴└─┴ ┴  ╚═╝┴└─┴ ┴└┴┘┴─┘└─┘┴└─
     """+Fore.RESET)
     print(Fore.GREEN + Style.NORMAL  +'Developed by Aman pachauri (paradox47.blogspot.com)'+Fore.RESET)
-    print(Fore.BLUE + Style.NORMAL+'\t\t\t\tversion [stable-1.2]\n\n'+ Fore.RESET)
+    print(Fore.BLUE + Style.NORMAL+'\t\t\t\tversion [2.4-stable]\n\n'+ Fore.RESET)
 
 def main() :
     banner()
@@ -221,19 +389,35 @@ def main() :
     parser.add_option("--domain",type="string",dest="domain",action="store",help="Specify domain for specific results eg IN,example.com")
     parser.add_option("--output",type="string",dest="ofile",metavar="file.txt",help='''Save ouput to file''')
     parser.add_option("--threads",type="int",dest="processes",metavar="2",default="7",help="Number of parallel processes")
+    group_injection = OptionGroup(parser, "Injection Test")
+    group_injection.add_option("--sqli",dest='sqli',action="store_true",help="Checks gathered urls for classic sql injection")
+    group_injection.add_option("--custom-payload",dest="c_payload",action="store",metavar=" ", help="Custom payload to test against URLs")
+    parser.add_option_group(group_injection)
     group_misc = OptionGroup(parser, "Misc")
-    group_misc.add_option("--filter", dest="filter", action="store_true", help="Turn on URL filter(gives specific result)")
+    group_misc.add_option("--no-filter", dest="filter", action="store_true", help="Turn Off URL filter(gives interesting results)")
+    group_misc.add_option("-v", dest="verbose", action="store_true", help="Display error messages on screen")
     parser.add_option_group(group_misc)
     (options, args) = parser.parse_args()
+    engine = options.engine
+    search = options.dork
+    pages = options.pages
+    domain = options.domain
+    filter_op = options.filter
+    processes = options.processes
+    file = options.ofile
+    sqli_op = options.sqli
+    custom_payload = options.c_payload
+    verbose = options.verbose
+    if options.sqli and options.ofile:
+        parser.error("options --sqli and --output should not used together")
+        exit()
+    if options.sqli :
+        display = 0
+    else :
+        display = 1
     if options.dork != None :
-        engine = options.engine
-        search = options.dork
-        pages = options.pages
-        domain = options.domain
-        filter_op = options.filter
-        processes = options.processes
-        file = options.ofile
         msg.info('Starting Crawler')
+        sleep(1)
         msg.blue('[ENGINE] '+ engine)
         msg.warning('Excess use of this tool can get your IP address banned and you may get 0 or false results ')
         start = timer()
@@ -241,97 +425,80 @@ def main() :
             if domain != None :
                 search = str(search + ' SITE:'+ domain)
             result_c = google_c.dork_scanner(search, pages, processes)
-            result = google.dork_scanner(search, pages, processes)
-            if not result or not result_c :
+            result_g = google.dork_scanner(search, pages, processes)
+            result = result_c + result_g
+            if not result :
                 msg.error('It seems like google has BANNED your IP ADDRESS. we recommend you use different engine [--engine=] or search google using browser')
-                msg.shutdown()
             else :
-                if filter_op == True :
-                    msg.info('Starting filter')
-                    sleep(1)
+                if filter_op == None :
                     result = filter(result)
-                    result_c = filter(result_c)
-                    for x in result :
-                        print('[LINK] '+ x)
-                    for x in result_c :
-                        print('[LINK] '+ x)
-
-                else :
-                    for x in result :
-                        print('[LINK] '+ x )
-                    for x in result_c :
-                        print('[LINK] '+ x)
-                msg.info('Total Url Grabbed : '+ str(len(result) + len(result_c)))
-                msg.info('Total time taken : '+ str((timer() - start)))
-                if options.ofile != None :
+                if sqli_op == True :
+                    list = sqli.inject(result, custom_payload, verbose)
+                    append_sqli(list)
+                    msg.info('Urls written to sqli_confirmed')
+                if file != None :
                     save_output(result, file)
-                    msg.info(' Urls written to '+ file)
-                msg.shutdown()
+                    msg.info('Urls written to '+ file)
+            msg.shutdown()
         elif engine == "bing" :
             if domain != None :
                 search = str(search + ' SITE:'+ domain)
             result = bing.dork_scanner(search, pages, processes)
             if not result :
                 msg.error('It seems like bing has BANNED your IP ADDRESS. we recommend you use different engine [--engine=] or search bing using browser')
-                msg.shutdown()
             else:
-                if filter_op == True :
-                    msg.info('Starting filter')
+                if filter_op != True :
                     result = filter(result)
-                    for x in result :
-                        print('[LINK] '+ x)
-                else :
-                    for x in result :
-                        print('[LINK] '+ x)
-                msg.info('Total Url Grabbed : '+ str(len(result)))
-                msg.info('Total time taken : '+ str((timer() - start)))
-                if options.ofile != None :
+                if sqli_op == True :
+                    list = sqli.inject(result, custom_payload, verbose)
+                    append_sqli(list)
+                    msg.info('Urls written to sqli_confirmed')
+                if display == 1 :
+                    print_list(result)
+                if file != None :
                     save_output(result, file)
-                    msg.info(' Urls written to '+ file)
-                msg.shutdown()
+                    msg.info('Urls written to '+ file)
+            msg.shutdown()
         elif engine == "all" :
             if domain != None :
                 search = str(search + ' SITE:'+ domain)
             result_wc = webcrawler.dork_scanner(search,pages,processes)
             result_b = bing.dork_scanner(search, pages, processes)
-            if filter_op == True :
-                msg.info('Starting filter')
-                result_f = result_b + result_wc
-                result_f = filter(result_f)
-                for x in result_f :
-                    print('[LINK] '+ x)
+            result = result_wc + result_b
+            if not result :
+                msg.error('It seems like your IP ADDRESS is BANNED. we recommend you use different engine [--engine=] or use browser')
             else :
-                for x in result_f :
-                    print('[LINK] '+ x )
-            msg.info('Total Url Grabbed : '+ str(len(result_b) + len(result_wc)))
-            msg.info('Total time taken : '+ str((timer() - start)))
-            if options.ofile != None :
-                save_output(result_wc, file)
-                save_output(result_b, file)
-                msg.info(' Urls written to '+ file)
+                if filter_op != True :
+                    result = filter(result)
+                if sqli_op == True :
+                    list = sqli.inject(result, custom_payload, verbose)
+                    append_sqli(list)
+                    msg.info('Urls written to sqli_confirmed')
+                if display == 1 :
+                    print_list(result)
+                if file != None :
+                    save_output(result, file)
+                    msg.info('Urls written to '+ file)
             msg.shutdown()
         elif engine == "webcrawler" :
             if domain != None :
                 search = str(search + ' SITE:'+ domain)
             result = webcrawler.dork_scanner(search, pages, processes)
             if not result :
-                msg.error('It seems like Web-crawler has BANNED your IP ADDRESS. we recommend you use different engine [--engine=] or search use browser')
-                msg.shutdown()
+                msg.error('It seems like Web-crawler has BANNED your IP ADDRESS. we recommend you use different engine [--engine=] or search webcrawler using browser')
             else:
-                if filter_op == True :
-                    msg.info('Starting filter')
+                if filter_op != True :
                     result = filter(result)
-                    for x in result :
-                        print('[LINK] '+ x)
-                else :
-                    for x in result :
-                        print('[LINK] '+ x)
-                msg.info('Total Url Grabbed : '+ str(len(result)))
-                msg.info('Total time taken : '+ str((timer() - start)))
-                if options.ofile != None :
+                if sqli_op == True :
+                    list = sqli.inject(result, custom_payload, verbose)
+                    append_sqli(list)
+                    msg.info('Urls written to sqli_confirmed')
+                if display == 1 :
+                    print_list(result)
+                if file != None :
                     save_output(result, file)
-                    msg.info(' Urls written to '+ file)
-                msg.shutdown()
+                    msg.info('Urls written to '+ file)
+            msg.shutdown()
         else :
             msg.error("\nInvalid Engine")
             msg.shutodwn()
